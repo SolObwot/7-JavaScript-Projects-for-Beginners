45d4f6927a2df247deb08e6ea00e3eeba84aff4d
Sourav Mandal <87852919+souravcodeweb@users.noreply.github.com>
2022-05-10 11:15:39 +0530

    Add files via upload

---
+++ Bulb on Bulb Off/app.js
@@ -0,0 +1,9 @@
+function bulbOn(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "img/ONbulb-removebg-preview.png")
+}
+
+function bulbOff(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "img/OFFbulb-removebg-preview.png")
+}
